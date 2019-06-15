49e9ee4987b13395347fcda67c38dab47a354456
andrewfoghel <andrewfoghel@gmail.com>
2019-06-14 20:27:18 -0400

    create models

---
+++ StartupWeekWorkshopiOS/ToDo App/ToDo App/Models/Coordinate.swift
@@ -0,0 +1,15 @@
+//
+//  Coordinate.swift
+//  ToDo App
+//
+//  Created by Andrew Foghel on 6/14/19.
+//  Copyright © 2019 Erik Lasky. All rights reserved.
+//
+
+import Foundation
+
+// TODO: may want to make these doubles (+ for N, - for S)
+struct Coordinate: Codable {
+    var lat: String?
+    var lon: String?
+}
---
+++ StartupWeekWorkshopiOS/ToDo App/ToDo App/Models/Reminder.swift
@@ -0,0 +1,17 @@
+//
+//  Reminder.swift
+//  ToDo App
+//
+//  Created by Andrew Foghel on 6/14/19.
+//  Copyright © 2019 Erik Lasky. All rights reserved.
+//
+
+import Foundation
+
+struct Reminder: Codable, Equatable {
+    var title: String?
+    var description: String?
+    
+    
+    
+}
